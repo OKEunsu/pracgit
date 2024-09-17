e037cdde4ac888b48d7cb6cdab02f9bc9fe7ab8b
OKEunsu <esok0617@gmail.com>
2024-09-18 00:15:41 +0900

    Revert "print twcie"
    This reverts commit 350b86814074c1b2926b2ab272f2eed221cbb4aa.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # test-repo 
 test-repo test
 hello, welcome to the world
+
--- main.py
+++ main.py
@@ -7,4 +7,3 @@ def print_hi(name_):
 # 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_hi('PyCharm')
